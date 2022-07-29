b6286ba49dd0156f9cac99c01a3ec2146e4c46bc
MWI <mjpru78692@gmail.com>
2022-07-29 10:22:38 -0700

    Version 1.0

---
+++ app/src/main/java/in/wizion/birthdaygreet/MainActivity.kt
@@ -0,0 +1,24 @@
+package `in`.wizion.birthdaygreet
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun createBirthdayCard(view: View) {
+
+        val name = nameInput.editableText.toString()
+
+        val intent = Intent(this, BirthdayGreetingActivity::class.java)
+        intent.putExtra(BirthdayGreetingActivity.NAME_EXTRA, name)
+        startActivity(intent)
+    }
+
+}
